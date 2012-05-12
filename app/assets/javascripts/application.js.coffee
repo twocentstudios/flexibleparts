28cac5d65f01433e36c4abec2986275c5a2a8309
Christopher Trott <chris@twocentstudios.com>
2012-05-12 13:57:47 -0500

    AppDrone: flexibleparts

---
+++ app/assets/javascripts/application.js.coffee
@@ -0,0 +1,8 @@
+//= require jquery
+//= require jquery_ujs
+//= require init
+//= require bootstrap-modal
+//= require chosen-jquery
+//= require jquery.html5-placeholder-shim
+//= require jquery_nested_form
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flexibleparts::Application.config.secret_token = '78f4c4951b2c633a49b97fbe4417fff33fa4777ccc9f6f1f342eb3651312c1504279f29fc85ff14ff7c08d9e278c2350315deef77aeebb68081680fe3a667606'
