25df328d38e7e2d28bee11fb05be4d983578fb4f
Christopher Trott <chris@twocentstudios.com>
2012-05-12 17:35:23 -0500

    Added parts and root to routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Flexibleparts::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
+  resources :parts
+
+  root to: 'parts#index'
+
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
